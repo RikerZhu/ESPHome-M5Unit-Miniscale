2961faf6a2ede36c1885d82a1bb1c9fc5525bc6d
kur1k0 <zhuzirun@m5stack.com>
2025-08-22 16:07:23 +0800

    change to __init__.py
    Signed-off-by: kur1k0 <zhuzirun@m5stack.com>

--- components/m5_unit_miniscale/sensor.py
+++ components/m5_unit_miniscale/sensor.py
@@ -7,7 +7,7 @@ CONF_ADDR = "address"
 
 m5_unit_miniscale_ns = cg.esphome_ns.namespace("m5_unit_miniscale")
 M5UnitMiniScale = m5_unit_miniscale_ns.class_(
-    "M5UnitMiniScale", sensor.Sensor, cg.PollingComponent
+    "M5UnitMiniScale", cg.Component, sensor.Sensor
 )
 
 M5UnitMiniScale = M5UnitMiniScale.method(
@@ -30,6 +30,5 @@ async def to_code(config):
     cg.add(var.set_i2c_params(config[CONF_SDA], config[CONF_SCL], config[CONF_ADDR]))
     await sensor.register_sensor(var, config)
 
-    # 在这里让 PlatformIO 自动拉 Arduino 库
     cg.add_library("Wire", None)
     cg.add_library("https://github.com/m5stack/M5Unit-Miniscale.git", None)
